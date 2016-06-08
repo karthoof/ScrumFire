8540eeee1a9288b49e6bc32cd013052b97535c30
Artem Kopytok <ar.bass@icloud.com>
2016-06-09 00:47:22 +0300

    new file:   mvnw new file:   mvnw.cmd new file:   pom.xml new file:   src/main/java/com/digdes/scrum/ScrumprojectApplication.java new file:   src/main/java/com/digdes/scrum/ServletInitializer.java new file:   src/main/resources/application.properties new file:   src/test/java/com/digdes/scrum/ScrumprojectApplicationTests.java

---
+++ src/main/java/com/digdes/scrum/ScrumprojectApplication.java
@@ -0,0 +1,12 @@
+package com.digdes.scrum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumprojectApplication.class, args);
+	}
+}
